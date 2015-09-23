5c0f082d4b5401d2d7ae96367bddaa1e5374a8dd
Marc R. Preston <phyrenight@gmail.com>
2015-09-23 13:58:50 -0700

    update

--- media.py
+++ media.py
@@ -1,8 +1,9 @@
 class Movie():
-	def __init__(self, title, poster_image,movie_trailer, description,
-				 year_made):
-		self.title = title
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = movie_trailer
-		self.description = description
-		self.year_made = year_made
+	def __init__(self, title, poster_image, movie_trailer,
+                 description, year_made):
+            self.title = title
+            self.poster_image_url = poster_image
+            self.trailer_youtube_url = movie_trailer
+            self.description = description
+            self.year_made = year_made
+            # year_made will contain the year the movie hit the theaters
