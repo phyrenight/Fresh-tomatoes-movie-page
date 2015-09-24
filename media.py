d9aa053534e102f518ac42c0c36c395ef455356a
Marc R. Preston <phyrenight@gmail.com>
2015-09-23 17:32:29 -0700

    update

--- media.py
+++ media.py
@@ -1,6 +1,15 @@
 class Movie():
+    """This class can hold a
+    movies information.The information
+    it can contain is very limited.
+    """
 	def __init__(self, title, poster_image, movie_trailer,
                  description, year_made):
+            """ This constructor method takes
+            input for the title, poster image,
+            a description, and the year the
+            movie came out.It does not output anything.
+            """
             self.title = title
             self.poster_image_url = poster_image
             self.trailer_youtube_url = movie_trailer
